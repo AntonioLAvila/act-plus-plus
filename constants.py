49f1df3e94568438df3c9c8c01382cfb2432a44d
Antonio Avila <antonioavila433@gmail.com>
2025-07-22 10:44:47 -0700

    right episode length

--- constants.py
+++ constants.py
@@ -34,7 +34,7 @@ else:
 controller_config = {
     'ckpt_dir': '/home/' + user + '/ckpts/pick_up_wafer_B16',              # from you
     'camera_names': ['cam_high', 'cam_left_wrist', 'cam_right_wrist'],  # from TASK_CONFIG (always going to be this though)
-    'episode_len': 800,                                                 # from TASK_CONFIG
+    'episode_len': 550,                                                 # from TASK_CONFIG
     'temporal_agg': False,                                              # up to you, only affects runtime
     'chunk_size': 100,                                                  # set at training time
     'hidden_dim': 512,                                                  # set at training time
